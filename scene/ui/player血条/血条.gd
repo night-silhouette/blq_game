99012eec9fe5f7548df9ef613b2b89ec0e6d9d5a
night <631434665@qq.com>
2025-11-03 20:09:34 +0800

    血条tween

--- scene/ui/player血条/血条.gd
+++ scene/ui/player血条/血条.gd
@@ -4,19 +4,22 @@ var ui_name="player血条"
 var max_hp:float:
 	set(value):
 		max_hp=value
-		textureProgressBar.max_value=max_hp
-@onready var textureProgressBar=$TextureProgressBar
+		textureProgressBar_up.max_value=max_hp
+		textureProgressBar_under.max_value=max_hp
+@onready var textureProgressBar_up=$TextureProgressBar_up
+@onready var textureProgressBar_under=$TextureProgressBar_under
 @onready var up=$up
 @onready var under=$under
 
 func tween_transform(value):
 	var tween:Tween=get_tree().create_tween()
-	tween.tween_property(textureProgressBar,"value",value,0.2)
+	tween.tween_property(textureProgressBar_up,"value",value,0.4)
 	tween.set_trans(Tween.TRANS_EXPO)
 	
 var now_hp:float:
 	set(value):
 		now_hp=value
+		textureProgressBar_under.value=value
 		tween_transform(value)
 		
 		
